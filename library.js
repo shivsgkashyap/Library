e27438bfe3ffdfa15a7c274021f34b95a1543f53
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-11-11 09:58:35 +0000

    Added render function + default data

--- library.js
+++ library.js
@@ -1,34 +1,73 @@
+let myLibrary = [];
+const DEFAULT_DATA = [
+  { name: "The Lord of the Rings", author: "Tolkien", status: "read" },
+  {
+    name: "Alice in Wonderland",
+    author: "Lewis Caroll",
+    status: "not read",
+  },
+  { name: "Naruto", author: "Masashi Kishimoto", status: "read" },
+];
 const bookName = document.querySelector("#name");
 const bookAuthor = document.querySelector("#author");
 const bookStatus = document.querySelector("#status");
 const bookPages = document.querySelector("#pages");
 const entryButton = document.querySelector(".entry-button");
 const formInputs = document.querySelector("form");
-
-let myLibrary = [];
+const formGrid = document.querySelector("#book-table-body");
 
 formInputs.addEventListener("submit", (e) => {
   e.preventDefault();
   addBookToLibrary();
+  render();
+  clearForm();
 });
 
 class Book {
-  constructor(name, author, title, pages, status) {
+  constructor(name, author, pages, status) {
     this.name = name;
     this.author = author;
-    this.title = title;
     this.pages = pages;
     this.status = status;
   }
 }
 
 function addBookToLibrary() {
-  if (bookName.value.length === 0 || bookAuthor.value.length === 0) {
+  if (
+    bookName.value.length === 0 ||
+    bookAuthor.value.length === 0 ||
+    bookPages.value.length === 0
+  ) {
     alert("Please, fill all the fields");
     return;
   }
-  const newBook = new Book(bookName.value, bookAuthor.value, bookStatus.value);
+  const newBook = new Book(
+    bookName.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookStatus.value
+  );
 
   myLibrary.push(newBook);
-  console.log(newBook);
+  console.log(myLibrary);
+}
+
+function clearForm() {
+  bookName.value = "";
+  bookAuthor.value = "";
+  bookPages.value = "";
+}
+
+function render() {
+  myLibrary.forEach((book) => {
+    const htmlBook = `
+      <tr>
+        <td>${book.name}</td>
+        <td>${book.author}</td>
+        <td><button class="status-button">${book.status}</button></td>
+        <td><button class="delete">delete</button></td>
+      </tr>
+      `;
+    formGrid.insertAdjacentHTML("afterbegin", htmlBook);
+  });
 }
